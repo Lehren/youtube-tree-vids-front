7b3cd7a599d7a1d213357c34857e4e4a940f03b5
lehren <loek-ehren@hotmail.com>
2017-06-03 16:50:02 +0200

    Rewrite App.js with bootstrap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import { Col, Grid, Navbar, Row } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+            <Navbar staticTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="/">YouTrees</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+            </Navbar>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
