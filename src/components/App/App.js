fc09e114221124e9bb69bd4137ea2641b56149b2
lehren <loek-ehren@hotmail.com>
2017-06-10 03:26:39 +0200

    Add prettier, lint-staged and husky and run prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { Col, Grid, Navbar, Row } from 'react-bootstrap';
-import IntroText from '../IntroText/IntroText';
-import './App.css';
+import React, { Component } from "react";
+import { Col, Grid, Navbar, Row } from "react-bootstrap";
+import IntroText from "../IntroText/IntroText";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <div>
-            <Navbar staticTop>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="/">YouTrees</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle/>
-                </Navbar.Header>
-            </Navbar>
-            <Grid>
-                <Row>
-                    <Col md={12}>
-                        <IntroText/>
-                    </Col>
-                </Row>
-            </Grid>
-        </div>
+      <div>
+        <Navbar staticTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">YouTrees</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <IntroText />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
