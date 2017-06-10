2e784e3ca2371cd31b459161cef251e210fbf342
lehren <loek-ehren@hotmail.com>
2017-06-10 16:21:37 +0200

    Lift state up from Inputbar to SearchDisplayYoutube comp

--- src/components/InputBar/InputBar.js
+++ src/components/InputBar/InputBar.js
@@ -2,31 +2,15 @@ import React, { Component } from "react";
 import { FormControl, FormGroup } from "react-bootstrap";
 
 class InputBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    });
-  }
-
   render() {
     return (
       <form>
         <FormGroup controlId="formInput">
           <FormControl
             type="text"
-            value={this.state.value}
+            value={this.props.value}
             placeholder={this.props.text}
-            onChange={this.handleChange}
-            onKeyPress={this.props.onKeyPress}
+            onChange={this.props.onChange}
           />
         </FormGroup>
       </form>
