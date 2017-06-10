d8eeda1f0b9d654087b635d34532e98e5a45ca6a
lehren <loek-ehren@hotmail.com>
2017-06-10 16:24:45 +0200

    Remove form from Inputbar component

--- src/components/InputBar/InputBar.js
+++ src/components/InputBar/InputBar.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
-import { FormControl, FormGroup } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
 
 class InputBar extends Component {
   render() {
     return (
-      <form>
-        <FormGroup controlId="formInput">
-          <FormControl
-            type="text"
-            value={this.props.value}
-            placeholder={this.props.text}
-            onChange={this.props.onChange}
-          />
-        </FormGroup>
-      </form>
+      <FormControl
+        type="text"
+        value={this.props.value}
+        placeholder={this.props.text}
+        onChange={this.props.onChange}
+      />
     );
   }
 }
